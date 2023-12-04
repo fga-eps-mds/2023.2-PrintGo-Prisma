81513a1893dbf9d32eb19762af681021888d14d3
Natanael Fernandes <filhonatanael01@gmail.com>
2023-12-04 16:26:31 -0300

    adicionando pipeline base de dev

--- prisma/migrations/20231204191650_create_schema/migration.sql
+++ prisma/migrations/20231204191650_create_schema/migration.sql
@@ -1,8 +1,11 @@
 -- CreateEnum
-CREATE TYPE "EmpressoraStatus" AS ENUM ('ATIVO', 'DESATIVADO');
+CREATE TYPE "Cargo" AS ENUM ('USER', 'ADMIN', 'LOCADORA');
 
 -- CreateEnum
-CREATE TYPE "Cargo" AS ENUM ('USER', 'ADMIN', 'LOCADORA');
+CREATE TYPE "ImpressoraStatus" AS ENUM ('ATIVO', 'DESATIVADO');
+
+-- CreateEnum
+CREATE TYPE "PadraoStatus" AS ENUM ('ATIVO', 'DESATIVADO');
 
 -- CreateTable
 CREATE TABLE "users" (
@@ -25,14 +28,15 @@ CREATE TABLE "padroes" (
     "modelo" STRING NOT NULL,
     "numeroSerie" STRING NOT NULL,
     "versaoFirmware" STRING NOT NULL,
-    "tempoAtivoSistema" TIMESTAMP(3) NOT NULL,
-    "totalDigitalizacoes" INT4 NOT NULL,
-    "totalCopiasPB" INT4 NOT NULL,
-    "totalCopiasColoridas" INT4 NOT NULL,
-    "totalImpressoesPb" INT4 NOT NULL,
-    "totalImpressoesColoridas" INT4 NOT NULL,
-    "totalGeral" INT4 NOT NULL,
+    "totalDigitalizacoes" STRING NOT NULL,
+    "totalCopiasPB" STRING NOT NULL,
+    "totalCopiasColoridas" STRING NOT NULL,
+    "totalImpressoesPb" STRING NOT NULL,
+    "totalImpressoesColoridas" STRING NOT NULL,
+    "totalGeral" STRING NOT NULL,
     "enderecoIp" STRING NOT NULL,
+    "status" "PadraoStatus" NOT NULL DEFAULT 'ATIVO',
+    "tempoAtivoSistema" STRING NOT NULL,
 
     CONSTRAINT "padroes_pkey" PRIMARY KEY ("id")
 );
@@ -40,15 +44,16 @@ CREATE TABLE "padroes" (
 -- CreateTable
 CREATE TABLE "impressoras" (
     "id" STRING NOT NULL,
-    "padrao_id" STRING NOT NULL,
-    "ip" STRING NOT NULL,
-    "numeroSerie" STRING NOT NULL,
     "codigoLocadora" STRING NOT NULL,
     "contadorInstalacao" INT4 NOT NULL,
     "dataInstalacao" TIMESTAMP(3) NOT NULL,
     "dataUltimoContador" TIMESTAMP(3) NOT NULL,
-    "status" "EmpressoraStatus" NOT NULL DEFAULT 'ATIVO',
+    "ip" STRING NOT NULL,
+    "numeroSerie" STRING NOT NULL,
+    "padrao_id" STRING NOT NULL,
     "unidadeId" STRING,
+    "locadora_id" STRING,
+    "status" "ImpressoraStatus" NOT NULL DEFAULT 'ATIVO',
 
     CONSTRAINT "impressoras_pkey" PRIMARY KEY ("id")
 );
@@ -65,14 +70,32 @@ CREATE TABLE "impressoes" (
     CONSTRAINT "impressoes_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "contadores" (
+    "id" STRING NOT NULL,
+    "numeroSerie" STRING NOT NULL,
+    "contadorCopiasPB" STRING NOT NULL,
+    "contadorImpressoesPB" STRING NOT NULL,
+    "contadorCopiasColoridas" STRING,
+    "contadorImpressoesColoridas" STRING,
+    "contadorGeral" STRING NOT NULL,
+    "dataHoraEmissaoRelatorio" TIMESTAMP(3) NOT NULL,
+    "pdfAnexo" STRING,
+
+    CONSTRAINT "contadores_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "impressoras_ip_key" ON "impressoras"("ip");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "impressoras_numeroSerie_key" ON "impressoras"("numeroSerie");
+
 -- AddForeignKey
-ALTER TABLE "impressoras" ADD CONSTRAINT "impressoras_codigoLocadora_fkey" FOREIGN KEY ("codigoLocadora") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "impressoras" ADD CONSTRAINT "impressoras_locadora_id_fkey" FOREIGN KEY ("locadora_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "impressoras" ADD CONSTRAINT "impressoras_padrao_id_fkey" FOREIGN KEY ("padrao_id") REFERENCES "padroes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -82,3 +105,6 @@ ALTER TABLE "impressoes" ADD CONSTRAINT "impressoes_impressora_id_fkey" FOREIGN 
 
 -- AddForeignKey
 ALTER TABLE "impressoes" ADD CONSTRAINT "impressoes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "contadores" ADD CONSTRAINT "contadores_numeroSerie_fkey" FOREIGN KEY ("numeroSerie") REFERENCES "impressoras"("numeroSerie") ON DELETE RESTRICT ON UPDATE CASCADE;
