53b365eb5e4ba1b92ea722cbb2f9f7ac687d7c7c
DaviMatheus <matdavimat@gmail.com>
2023-12-06 01:31:37 -0300

    🔥adding_new_values_printer

---
+++ prisma/migrations/20231206043000_new_values_printer/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "impressoras" ADD COLUMN     "contadorRetirada" STRING;
+ALTER TABLE "impressoras" ADD COLUMN     "dataRetirada" STRING;
+ALTER TABLE "impressoras" ADD COLUMN     "ultimoContador" STRING;
